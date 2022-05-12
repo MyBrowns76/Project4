b864ca41aceb9a16d403bb633724e8758c97af42
MyBrowns76 <eddiecsworld76@yahoo.com>
2022-05-12 16:40:46 -0400

    Commit time 8

--- Desktop/Project 4/speech-input.js
+++ Desktop/Project 4/speech-input.js
@@ -80,6 +80,25 @@ else {
   console.log("Your Browser does not support speech Recognition");
   info.textContent = "Your Browser does not support Speech Recognition";
 }
+ //Get the button
+ var mybutton = document.getElementById("myBtn");
+
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+ } else {
+    mybutton.style.display = "none";
+ }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+ }
 
 
 
