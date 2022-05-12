b79c8881a53c614e7b6abe103056083396becfaa
MyBrowns76 <eddiecsworld76@yahoo.com>
2022-05-11 20:46:22 -0400

    Push time 5

--- Desktop/Project 4/speech-input.js
+++ Desktop/Project 4/speech-input.js
@@ -1,30 +1,147 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+const searchForm = document.querySelector("#search-form");
+const searchFormInput = searchForm.querySelector("input"); // <=> document.querySelector("#search-form input");
+const info = document.querySelector(".info");
 
-const recognition = new SpeechRecognition();
-recognition.lang = 'en-US';
+// The speech recognition interface lives on the browser’s window object
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition; // if none exists -> undefined
 
-const textField = document.getElementById('test');
-const btn = document.getElementById('btn');
-btn.addEventListener('click', () => {
-  recognition.start();
-  console.log('Ready for a voice command');
-});
+if(SpeechRecognition) {
+  console.log("Your Browser supports speech Recognition");
+  
+  const recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  // recognition.lang = "en-US";
 
-recognition.onresult = function(event) {
-  var command = event.results[0][0].transcript;
-  textField.value = command;
-  console.log("Command: " + command);
+  searchForm.insertAdjacentHTML("beforeend", '<button type="button"><i class="fas fa-microphone"></i></button>');
+  searchFormInput.style.paddingRight = "50px";
+
+  const micBtn = searchForm.querySelector("button");
+  const micIcon = micBtn.firstElementChild;
+
+  micBtn.addEventListener("click", micBtnClick);
+  function micBtnClick() {
+    if(micIcon.classList.contains("fa-microphone")) { // Start Voice Recognition
+      recognition.start(); // First time you have to allow access to mic!
+    }
+    else {
+      recognition.stop();
+    }
+  }
+
+  recognition.addEventListener("start", startSpeechRecognition); // <=> recognition.onstart = function() {...}
+  function startSpeechRecognition() {
+    micIcon.classList.remove("fa-microphone");
+    micIcon.classList.add("fa-microphone-slash");
+    searchFormInput.focus();
+    console.log("Voice activated, SPEAK");
+  }
+
+  recognition.addEventListener("end", endSpeechRecognition); // <=> recognition.onend = function() {...}
+  function endSpeechRecognition() {
+    micIcon.classList.remove("fa-microphone-slash");
+    micIcon.classList.add("fa-microphone");
+    searchFormInput.focus();
+    console.log("Speech recognition service disconnected");
+  }
+
+  recognition.addEventListener("result", resultOfSpeechRecognition); // <=> recognition.onresult = function(event) {...} - Fires when you stop talking
+  function resultOfSpeechRecognition(event) {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    
+    if(transcript.toLowerCase().trim()==="stop recording") {
+      recognition.stop();
+    }
+    else if(!searchFormInput.value) {
+      searchFormInput.value = transcript;
+    }
+    else {
+      if(transcript.toLowerCase().trim()==="go") {
+        searchForm.submit();
+      }
+      else if(transcript.toLowerCase().trim()==="reset input") {
+        searchFormInput.value = "";
+      }
+      else {
+        searchFormInput.value = transcript;
+      }
+    }
+    // searchFormInput.value = transcript;
+    // searchFormInput.focus();
+    // setTimeout(() => {
+    //   searchForm.submit();
+    // }, 500);
+  }
+  
+  info.textContent = 'Voice Commands: "stop recording", "reset input", "go"';
+  
+}
+else {
+  console.log("Your Browser does not support speech Recognition");
+  info.textContent = "Your Browser does not support Speech Recognition";
 }
 
-recognition.onspeechend = function() {
-  recognition.stop();
-}
-function voiceInputOver(val){
-  alert("Voice input is complete");
-  alert("Your input is " + val);
-}
-// recognition.onerror = function(event){
-//   console.log('error?');
-//   console.log(event);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+// var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+// const recognition = new SpeechRecognition();
+// recognition.lang = 'en-US';
+
+// const textField = document.getElementById('test');
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', () => {
+//   recognition.start();
+//   console.log('Ready for a voice command');
+// });
+
+// recognition.onresult = function(event) {
+//   var command = event.results[0][0].transcript;
+//   textField.value = command;
+//   console.log("Command: " + command );
+  
 // }
+
+// recognition.onspeechend = function() {
+//   recognition.stop();
+// }
+
+// // function voiceInputOver(val){
+// //   alert("Voice input is complete");
+// //   alert("Your input is " + val);
+// // }
+// // recognition.onerror = function(event){
+// //   console.log('error?');
+// //   console.log(event);
+// // }
